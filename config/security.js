96233deb1e1a8bc29685c8f638b7b72428e4150c
Loretta Bandera <lbandera@us.ibm.com>
2017-12-13 12:38:06 -0500

    Remove header and footer so demo can be included in iframe. Remove helmut package to prevent cross browser scripting problem. Minor styling changes to resize titles

--- config/security.js
+++ config/security.js
@@ -19,14 +19,12 @@
 // security.js
 var secure = require('express-secure-only');
 var rateLimit = require('express-rate-limit');
-var helmet = require('helmet');
 
 module.exports = function(app) {
 
   // 1. redirects http to https
   app.enable('trust proxy'); // required when running on bluemix or similar to know if users originally came in on HTTPS and avoid endless redirects
   app.use(secure());
-  app.use(helmet());
 
   // 3. rate limiting
   var translateLimiter = rateLimit({
